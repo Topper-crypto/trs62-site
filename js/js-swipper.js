0a4e874e61319dfc16c93f33ab6271c7bd55e983
Topper-crypto <tiadong@gmail.com>
2023-09-21 10:29:55 +0300

    fix

--- js/js-swipper.js
+++ js/js-swipper.js
@@ -4,14 +4,14 @@ const swiper = new Swiper('.swiper', {
   loop: true,
 
 
- effect: "cube",
+/*  effect: "cube",
 grabCursor: true,
 cubeEffect: {
   shadow: true,
   slideShadows: true,
   shadowOffset: 20,
   shadowScale: 0.94,
-}, 
+},  */
  
   
 
